95ff85b4f70c69c1ef56bff0e9819c66d5e95330
Adrien <148849224+AB-Ynov@users.noreply.github.com>
2023-11-16 16:04:03 +0100

    Create main.tf

---
+++ .terraform/providers/registry.terraform.io/hashicorp/main.tf
@@ -0,0 +1,35 @@
+provider "azurerm" {
+  features = {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = var.virtual_network_name
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = var.aks_cluster_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = var.aks_cluster_name
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+}
