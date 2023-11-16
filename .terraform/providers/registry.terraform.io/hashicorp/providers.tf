bf2c11b3a26155bfbc7a929680a156fce8fa10f4
Adrien <148849224+AB-Ynov@users.noreply.github.com>
2023-11-16 16:03:07 +0100

    Update providers.tf

--- .terraform/providers/registry.terraform.io/hashicorp/providers.tf
+++ .terraform/providers/registry.terraform.io/hashicorp/providers.tf
@@ -1,19 +1,3 @@
-# providers.tf
-
-# Spécification des fournisseurs Terraform et de leurs versions
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-terraform {
-  required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.0, < 4.0"
-    }
-  }
+provider "azurerm" {
+  features = {}
 }
