e25fc789966d76bd5b088ffcf3fe483067d17bea
Adrien <148849224+AB-Ynov@users.noreply.github.com>
2023-11-16 18:43:39 +0000

    Ecrivez le code Terraform qui va déployer sur Azure: Un resource group Un Virtual Network Un Subnet Un Azure Kubernetes Service
    Installer avec un Helm chart Ingress Controller Nginx
    Installer avec un Helm chart Redis
    Installer avec un Helm chart KubeCost

---
+++ terraform.tf
@@ -0,0 +1,50 @@
+# main.tf
+
+# main.tf
+
+provider "azurerm" {
+  features {}
+}
+
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myResourceGroup"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myVNet"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "mySubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "myAKSCluster"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "myaksdns"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = <<-EOT
+      Standard_DS2_v2
+    EOT
+  }
+
+  service_principal {
+    client_id     = var.client_id
+    client_secret = var.client_secret
+  }
+}
---
+++ terraform.tfvars
@@ -0,0 +1,6 @@
+# terraform.tfvars
+
+client_id       = "votre-id-client-azure"
+client_secret   = "votre-secret-client-azure"
+tenant_id       = "votre-id-tenant-azure"
+subscription_id = "votre-id-abonnement-azure"
